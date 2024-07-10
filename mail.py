a66e302f6bca680d1ab4b3464e30f9f9a6634684
vishwanath9502 <141599718+vishwanath9502@users.noreply.github.com>
2024-07-10 15:30:21 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pickle
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Display title
+image_path = 'Innomatics-logo.png'  # Replace with your actual PNG image file path
+
+# Display the PNG image
+st.image(image_path)
+spam_image_path = r"C:\Users\gorla\OneDrive\Desktop\Spam.png"  # Replace with your actual PNG image file path for spam
+st.title("Predicting Email Spam or Ham")
+
+
+model = pickle.load(open(r"C:\Users\gorla\model.pkl",'rb'))
+bow = pickle.load(open(r"C:\Users\gorla\bow.pkl",'rb'))
+
+
+email = st.text_input("Enter the email:")
+
+if st.button("Classify Email"):
+    if email:
+        # Transform input email using CountVectorizer
+        data = bow.transform([email]).toarray()
+        prediction = model.predict(data)[0]
+        # Display the prediction result
+        st.write(f"The email is classified as: **{prediction}**")
+        
+        # Display the spam image if classified as spam
+        if prediction == 'spam':
+            st.image(spam_image_path)
+
+
